1d9c07bd17654dd24fc3796778314b8eda43e7ad
OmbuiMark <john1738mark@gmail.com>
2024-09-30 14:29:32 -0400

    email change

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,8 +3,8 @@ export const personalData = {
   profile: '/profile.jpg',
   designation: "Software Development Agency",
   description: "At Nexus Agency, we are a team of passionate programmers dedicated to crafting innovative solutions. We embrace a self-learning culture, continually exploring new technologies to enhance our skills. With a strong focus on problem-solving, we specialize in web application development using various tech stacks, particularly JavaScript. We are always open to exciting job opportunities that align with our expertise and interests.",
-  email: 'contact@nexusagency.com',
-  phone: '+916306719831',
+  email: 'impressivecopywriter@gmail.com',
+  phone: '',
   address: '',
   github: 'https://github.com/nexusagency',
   facebook: 'https://www.facebook.com/nexusagency/',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/nexusagency',
   leetcode: "https://leetcode.com/nexusagency/",
   devUsername: "nexusagency",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: ""
 };
